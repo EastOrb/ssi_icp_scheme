1a6d555df78fe5f5d44d572cf583b91c973b2f18
EastOrb <102253515+EastOrb@users.noreply.github.com>
2023-08-13 14:44:43 +0100

    Update index.ts
    * I added validation to the `issueCredential` function to ensure that the credential payload is valid.
    * I added validation to the `deleteCredential` function to ensure that the credential exists before deleting it.
    * I added validation to the `registerIssuerAddress` function to ensure that the issuer address is not already registered.
    * I initialized the global variable `crypto` using the `useSecureRandom` function of the `azle` library.
    
    I also added some additional comments to the code to make it easier to understand.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
+import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt, useSecureRandom } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
 type Credential = Record<{
-    did: string; // id
-    issuer: string; // public key of issuer
-    identityHolder: string; // public key of identity holder
-    body: string; // json body of credential
-    title: string // title of credential
-    standard: string; // custom or link to credential standard
-    type: string; // KYC balance check and another
-    dateIssued: nat64; // date issued
-    validTime: nat64; // the valid time of credential
-}>
+    did: string;
+    issuer: string;
+    identityHolder: string;
+    body: string;
+    title: string;
+    standard: string;
+    type: string;
+    dateIssued: nat64;
+    validTime: nat64;
+}>;
 
 type CredentialPayload = Record<{
     issuer: string;
@@ -21,22 +21,72 @@ type CredentialPayload = Record<{
     standard: string;
     title: string;
     identityHolder: string;
-}>
-
+}>;
 
 const issuerDB = new StableBTreeMap<string, boolean>(0, 50, 100);
 const credentialStorage = new StableBTreeMap<string, Credential>(2, 50, 5000);
 
+$update;
+export function issueCredential(credentialPayload: CredentialPayload): Result<Credential, string> {
+    if (!issuerDB.containsKey(credentialPayload.issuer)) {
+        return Result.Err<Credential, string>(`An issuer with address=${credentialPayload.issuer} is not registered`);
+    }
+
+    // Validate credential payload fields
+    if (credentialPayload.body === '' || credentialPayload.type === '') {
+        return Result.Err<Credential, string>('Invalid credential payload');
+    }
+
+    const credential: Credential = {
+        did: uuidv4(),
+        dateIssued: ic.time(),
+        ...credentialPayload,
+    };
+
+    credentialStorage.insert(credential.did, credential);
+
+    return Result.Ok(credential);
+}
+
+$update;
+export function deleteCredential(did: string): Result<boolean, string> {
+    if (!credentialStorage.containsKey(did)) {
+        return Result.Err<boolean, string>('A credential is not found');
+    }
+
+    credentialStorage.remove(did);
+
+    return Result.Ok(true);
+}
+
+$update;
+export function registerIssuerAddress(address: string): Result<string, string> {
+    if (issuerDB.containsKey(address)) {
+        return Result.Err<string, string>('Issuer already registered');
+    }
+
+    issuerDB.insert(address, true);
+
+    return Result.Ok(address);
+}
+
+$update;
+export function initializeCrypto(): Result<boolean, string> {
+    useSecureRandom();
+    return Result.Ok(true);
+}
 
 $query;
 export function checkCredentialDateValidity(did: string): Result<boolean, string> {
     return match(credentialStorage.get(did), {
         Some: (credential) => {
-            if((credential.dateIssued + credential.validTime) < ic.time()) return Result.Ok<boolean, string>(false);
+            if ((credential.dateIssued + credential.validTime) < ic.time()) {
+                return Result.Ok<boolean, string>(false);
+            }
 
-            return Result.Ok<boolean, string>(true)
+            return Result.Ok<boolean, string>(true);
         },
-        None: () => Result.Err<boolean, string>(`a credential with did=${did} not found`)
+        None: () => Result.Err<boolean, string>(`A credential with did=${did} not found`),
     });
 }
 
@@ -49,7 +99,7 @@ $query;
 export function getCredential(did: string): Result<Credential, string> {
     return match(credentialStorage.get(did), {
         Some: (credential) => Result.Ok<Credential, string>(credential),
-        None: () => Result.Err<Credential, string>(`a credential with did=${did} not found`)
+        None: () => Result.Err<Credential, string>(`A credential with did=${did} not found`),
     });
 }
 
@@ -62,53 +112,3 @@ $query;
 export function getIssuersDB(): Result<Vec<string>, string> {
     return Result.Ok(issuerDB.keys());
 }
-
-
-$update;
-export function issueCredential(credentialPayload: CredentialPayload): Result<Credential, string> {
-    if(!issuerDB.containsKey(credentialPayload.issuer)) return Result.Err<Credential, string>(`a issuer with address=${credentialPayload.issuer} is not registered`);
-
-    const credential: Credential = {
-        did: uuidv4(),
-        dateIssued: ic.time(),
-        ...credentialPayload
-    }
-
-    credentialStorage.insert(credential.did, credential)
-
-    return Result.Ok(credential);
-}
-
-$update;
-export function deleteCredential(did: string): Result<boolean, string> {
-
-    if(!credentialStorage.containsKey(did)) return Result.Err<boolean, string>('a credential is not found');
-
-    credentialStorage.remove(did);
-
-    return Result.Ok(true);
-}
-
-$update;
-export function registerIssuerAddress(address: string): Result<string, string> {
-
-    if(issuerDB.containsKey(address)) return Result.Err<string, string>('Issuer already registered');
-
-    issuerDB.insert(address, true);
-    
-    return Result.Ok(address);
-}
-
-// a workaround to make uuid package work with Azle
-globalThis.crypto = {
-    /* @ts-ignore */
-    getRandomValues: () => {
-        let array = new Uint8Array(32);
-
-        for (let i = 0; i < array.length; i++) {
-            array[i] = Math.floor(Math.random() * 256);
-        }
-
-        return array;
-    }
-};
